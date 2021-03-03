2e9eeac644c44d7a6976cab3c62541552884a053
Maíra Costa <maira_mcs@hotmail.com>
2021-03-03 19:29:49 -0300

    first commit

---
+++ members-frontend/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import { ApiService } from './api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'members-frontend';
+
+  members = [
+    {name: 'Member 01', id: 1, surname:'Janis', photo: 'http://www.minhaapp.com/photo1'},
+    {name: 'Member 02', id: 2, surname:'Mary', photo: 'http://www.minhaapp.com/photo2'},
+    {name: 'Member 03', id: 3, surname:'Mujica', photo: 'http://www.minhaapp.com/photo3'},
+  ];
+
+  constructor(private api:ApiService) {
+    this.getMembers();
+  }
+
+  getMembers = () => {
+    this.api.getAllMembers().subscribe(
+      data => {
+        this.members = data
+      },
+      error => {
+        console.log('ERRO', error.massage);
+      }
+    );
+  };
+
+  memberClicked = (member) => {
+    this.api.getMember(member.id).subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        console.log('ERRO', error.message);
+      }
+    );
+  };
+}
